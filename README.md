40be5f4a921ed2cb9779382af5e8a4817faebe0d
autopilot-devops <144722249+autopilot-devops@users.noreply.github.com>
2023-09-11 23:26:59 +0530

    Adding Environment Setup Script
    Adding Environment Setup Script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ansible-hands-on
+
+Setup Environment Steps:
+
+1. Clone repo on amazon-linux-2 OS
+2. Navigate to setup directory
+3. sudo su
+4. chmod 700 env-setup.sh
+5. ./env-setup.sh
---
+++ setup/env-setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#Install Packages
+
+sudo yum -y update
+
+sudo yum -y install docker
+
+dnf install python3-pip -y
+
+pip install ansible
+
+yum -y groupinstall "Development Tools"
+
+sudo wget http://sourceforge.net/projects/sshpass/files/latest/download -O sshpass.tar.gz
+sudo tar -zxvf sshpass.tar.gz --strip-components=1
+sudo ./configure
+sudo make
+sudo make install
+
+
+#Start Docker
+sudo systemctl start docker
+
+
+
+if [ -f /etc/ansible/ansible.cfg ]
+
+then
+
+        sudo sed -i 's/#host_key_checking/host_key_checking/' /etc/ansible/ansible.cfg
+
+else
+
+        sudo mkdir /etc/ansible/
+
+        sudo touch /etc/ansible/ansible.cfg
+
+        sudo echo -e "[defaults]\nhost_key_checking = False" > /etc/ansible/ansible.cfg
+
+fi
+
+
+
+sudo docker pull autopilotdevops/ansible
+
+sudo docker run -itd --name container1 autopilotdevops/ansible
+
+sudo docker run -itd --name container2 autopilotdevops/ansible
+
+sudo docker run -itd --name container3 autopilotdevops/ansible
+
+
+
+count=1
+
+sudo docker ps -a -q | while read line;
+
+do
+
+IP=`sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $line`
+
+sudo echo "webserver${count} ansible_connection=ssh ansible_user=root ansible_password=Devops ansible_host=$IP" >> /root/inventory.txt
+
+count=$((count + 1))
+
+done
+
+sudo cd /root
+sudo cat /root/inventory.txt
